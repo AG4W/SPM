c95d66b852b6a3d6ee9689acd70ed4873e383935
AG4W <lightarts@live.se>
2020-04-06 14:10:45 +0200

    Add Locomotion Controller, Add Camera Controller, Rename scene.

---
+++ Assets/Source/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]float sensitivity = 2f;
+    [SerializeField]float translationSpeed = 5f;
+
+    [SerializeField]GameObject target;
+
+    void Update()
+    {
+        GatherInput();
+    }
+
+    void GatherInput()
+    {
+        this.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime);
+        this.transform.position = Vector3.Lerp(this.transform.position, target.transform.position, translationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Source/Character/LocomotionController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class LocomotionController : MonoBehaviour
+{
+    [SerializeField]Transform jig;
+
+    [SerializeField]Vector3 targetInput;
+
+    [SerializeField]float movementSpeed = 2f;
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = this.GetComponentInChildren<Animator>();
+    }
+    void Update()
+    {
+        GatherInput();
+
+        UpdateAnimator();
+        UpdatePosition();
+    }
+
+    void GatherInput()
+    {
+        targetInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+    }
+
+    void UpdatePosition()
+    {
+        this.transform.rotation = Quaternion.Euler(0f, jig.transform.eulerAngles.y, 0f);
+        //this.transform.position += animator.deltaPosition;
+
+        //Vector3 movement = targetInput.normalized;
+        //this.transform.position += ((movement.x * jig.right) + (movement.z * jig.forward)) * (targetInput.normalized.magnitude * movementSpeed) * Time.deltaTime;
+    }
+    void UpdateAnimator()
+    {
+        animator.SetFloat("x", targetInput.x);
+        animator.SetFloat("z", targetInput.z);
+    }
+}
