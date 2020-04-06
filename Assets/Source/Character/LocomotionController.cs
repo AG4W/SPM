6c016ed478b8d287b875948e89f8596d66aebbf8
AG4W <lightarts@live.se>
2020-04-06 16:46:37 +0200

    Add Exploration/Combat Locomotion Animation setups. Add Torch.

--- Assets/Source/Character/LocomotionController.cs
+++ Assets/Source/Character/LocomotionController.cs
@@ -2,9 +2,15 @@
 
 public class LocomotionController : MonoBehaviour
 {
+    [SerializeField]GameObject torch;
     [SerializeField]Transform jig;
 
     [SerializeField]Vector3 targetInput;
+
+    [SerializeField]float combatInterpolationSpeed = 10f;
+    [SerializeField]float explorationInterpolationSpeed = 2.5f;
+
+    [SerializeField]bool inCombatMode; 
 
     Animator animator;
 
@@ -22,11 +28,21 @@ public class LocomotionController : MonoBehaviour
 
     void GatherInput()
     {
+        if (Input.GetKeyDown(KeyCode.Tab))
+            ToggleMovementMode();
+        if (Input.GetKeyDown(KeyCode.F))
+            torch.SetActive(!torch.activeSelf);
+
         targetInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
     }
 
     void UpdatePosition()
     {
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(new Ray(this.transform.position, Vector3.down), out hit))
+        //    this.transform.position += new Vector3(0f, hit.distance, 0f);
+
         this.transform.rotation = Quaternion.Euler(0f, jig.transform.eulerAngles.y, 0f);
         //this.transform.position += animator.deltaPosition;
 
@@ -35,7 +51,13 @@ public class LocomotionController : MonoBehaviour
     }
     void UpdateAnimator()
     {
-        animator.SetFloat("x", targetInput.x);
-        animator.SetFloat("z", targetInput.z);
+        animator.SetFloat("x", targetInput.x, inCombatMode ? combatInterpolationSpeed : explorationInterpolationSpeed, Time.deltaTime);
+        animator.SetFloat("z", targetInput.z, inCombatMode ? combatInterpolationSpeed : explorationInterpolationSpeed, Time.deltaTime);
+    }
+
+    void ToggleMovementMode()
+    {
+        inCombatMode = !inCombatMode;
+        animator.SetTrigger("toggleMovementMode");
     }
 }
