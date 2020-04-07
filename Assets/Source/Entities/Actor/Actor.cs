c5077c329299f0aee5c3845fc60a076f2bc2f94e
zebbe994 <63231705+zebbe994@users.noreply.github.com>
2020-04-07 18:34:14 +0200

    Add NonPlayerActor, Change Actor

--- Assets/Source/Entities/Actor/Actor.cs
+++ Assets/Source/Entities/Actor/Actor.cs
@@ -10,6 +10,8 @@ public class Actor : Entity
 
     Vital[] vitals;
 
+    public Transform FocusPoint { get; private set; }
+
     // Använd inte Start/Awake i klasser som ärver ifrån Entity!
     // Använd istället protected override base.Initialize(), denna kallas i Start.
     // Kom ihåg att anropa basmetoden också.
@@ -19,6 +21,15 @@ public class Actor : Entity
 
         vitals = new Vital[Enum.GetNames(typeof(VitalType)).Length];
         vitals[(int)VitalType.Health] = new Vital(VitalType.Health, maxHealth, healthRegenerationRate, healthRegenerationAmount);
+
+        this.FocusPoint = this.transform.Find("focusPoint");
+        if (this.FocusPoint == null)
+        {
+            Debug.LogWarning(this.name + " is missing a focusPoint, using position of transform");
+            this.FocusPoint = new GameObject("focusPoint").transform;
+            this.FocusPoint.SetParent(this.transform);
+            this.FocusPoint.position = this.transform.position;
+        }
     }
 
     void Update()
