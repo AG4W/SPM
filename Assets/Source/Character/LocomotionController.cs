bc0c7f4f1f846c751879e1d2c9ce34768cd614e6
AG4W <lightarts@live.se>
2020-04-07 22:20:08 +0200

    Fix LocomotionController collision.

--- Assets/Source/Character/LocomotionController.cs
+++ Assets/Source/Character/LocomotionController.cs
@@ -6,12 +6,16 @@ public class LocomotionController : MonoBehaviour
     [SerializeField]Transform jig;
 
     [SerializeField]Vector3 targetInput;
+    [SerializeField]Vector3 correctedInput;
 
     [SerializeField]float targetStance;
 
-    [SerializeField]float groundedRaycastDistance = .2f;
     [SerializeField]bool isGrounded;
     [SerializeField]bool isSprinting;
+
+    [Header("Movement/Collision Properties")]
+    [SerializeField]float characterHeight = 1.8f;
+    [SerializeField]float characterRadius = .25f;
 
     [Header("Combat Mode")]
     [SerializeField]float explorationInterpolationSpeed = 2.5f;
@@ -27,30 +31,30 @@ public class LocomotionController : MonoBehaviour
 
     [Header("Collision Management")]
     [SerializeField]LayerMask collisionLayer;
-    CapsuleCollider chestCollider;
-
-    [SerializeField]Vector3 velocity;
-    [SerializeField]float airResistance = .4f;
 
     Animator animator;
 
-    [SerializeField]bool displayDebugGizmos = true;
-
     void Awake()
     {
-        chestCollider = GetComponent<CapsuleCollider>();
         animator = this.GetComponentInChildren<Animator>();
     }
     void Update()
     {
         //UpdateGroundedStatus();
-
         GatherInput();
-
-        WallCollision();
 
         UpdateAnimator();
         UpdateRotation();
+    }
+
+    void OnAnimatorMove()
+    {
+        this.transform.position += this.animator.deltaPosition;
+
+        Debug.DrawRay(this.transform.position, this.animator.deltaPosition);
+
+        if (Physics.CapsuleCast(this.transform.position, this.transform.position + (Vector3.up * characterHeight), characterRadius, this.animator.deltaPosition.normalized, out RaycastHit hit, this.animator.deltaPosition.magnitude + .5f))
+            this.transform.position += this.animator.deltaPosition.GetNormalForce(hit.normal);
     }
 
     void GatherInput()
@@ -64,8 +68,6 @@ public class LocomotionController : MonoBehaviour
         if (!isGrounded)
             return;
 
-        //if (Input.GetKeyDown(KeyCode.Tab))
-        //    ToggleMovementMode();
         if (Input.GetKeyDown(KeyCode.F))
             torch.SetActive(!torch.activeSelf);
         if (Input.GetKeyDown(KeyCode.Space))
@@ -82,19 +84,7 @@ public class LocomotionController : MonoBehaviour
 
     void UpdateRotation()
     {
-        //RaycastHit hit;
-
-        //if (Physics.Raycast(new Ray(this.transform.position, Vector3.down), out hit))
-        //    this.transform.position += new Vector3(0f, hit.distance, 0f);
-
-        if (inCombatMode)
-            this.transform.rotation = Quaternion.Euler(0f, jig.transform.eulerAngles.y, 0f);
-        else
-            this.transform.rotation = Quaternion.Euler(0f, this.transform.eulerAngles.y + (targetInput.x * explorationRotationSpeed), 0f);
-        //this.transform.position += animator.deltaPosition;
-
-        //Vector3 movement = targetInput.normalized;
-        //this.transform.position += ((movement.x * jig.right) + (movement.z * jig.forward)) * (targetInput.normalized.magnitude * movementSpeed) * Time.deltaTime;
+        this.transform.rotation = Quaternion.Euler(0f, jig.transform.eulerAngles.y, 0f);
     }
     void UpdateAnimator()
     {
@@ -126,26 +116,6 @@ public class LocomotionController : MonoBehaviour
     //    if(displayDebugGizmos)
     //        Debug.DrawLine(this.transform.position + (Vector3.up * .1f), this.transform.position + (Vector3.down * groundedRaycastDistance), hit.transform == null ? Color.red : Color.green);
     //}
-    void WallCollision()
-    {
-        Vector3 targetInputInWorldSpace = targetInput.TransformToLocalDirection(this.transform);
-        Vector3 Point1Capsule = chestCollider.transform.position + chestCollider.center + Vector3.up * (chestCollider.height / 2 - chestCollider.radius);
-        Vector3 Point2Capsule = chestCollider.transform.position + chestCollider.center + Vector3.down * (chestCollider.height / 2 - chestCollider.radius);
-        bool willHitWall = Physics.CapsuleCast(Point1Capsule, Point2Capsule, chestCollider.radius, targetInputInWorldSpace.normalized, out RaycastHit hitInfo, targetInput.magnitude + .1f, collisionLayer);
-        if (willHitWall)
-        {
-            targetInputInWorldSpace += animator.velocity.GetNormalForce(hitInfo.normal);
-        }
-        targetInput = targetInputInWorldSpace;
-        //bool hasHitWall = Physics.CapsuleCast(Point1Capsule, Point2Capsule, chestCollider.radius, animator.velocity.normalized, animator.velocity.magnitude + .1f, collisionLayer);
-        //if (displayDebugGizmos)
-        //    Debug.DrawRay(Point1Capsule, animator.velocity.normalized, hasHitWall == false ? Color.red : Color.green);
-        //if (hasHitWall)
-        //{
-            
-        //    this.transform.position -= animator.deltaPosition;
-        //}
-    }
 
     void Jump()
     {
--- Assets/Source/Extensions/Extensions.cs
+++ Assets/Source/Extensions/Extensions.cs
@@ -18,7 +18,7 @@ public static class Extensions
         float dot = Vector3.Dot(velocity, normal);
         // Om vår hastighet och normal pekar åt samma håll (dot = positiv), bör det inte finnas någon normalkraft.
         if (dot > 0f)
-            dot = 0f;
+            return Vector3.zero;
 
         Vector3 projection = dot * normal; // (Skalärprodukten mellan vektorn velocity och normaliserade vektorn normal) * normaliserade vektorn normal
         return -projection; // The normal force returned
