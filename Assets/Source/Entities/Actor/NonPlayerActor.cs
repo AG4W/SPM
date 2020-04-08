3d6eff0e709b22bf682041eebdcc4388467172ae
zebbe994 <63231705+zebbe994@users.noreply.github.com>
2020-04-08 13:27:15 +0200

    Add DroneA, DroneB Change NonPlayerActor

--- Assets/Source/Entities/Actor/NonPlayerActor.cs
+++ Assets/Source/Entities/Actor/NonPlayerActor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NonPlayerActor : Actor
 {
     [SerializeField] float detectionRange;
+    [Range(-0f, 180f)] [SerializeField] float detectionFieldOfView = .25f;
     [SerializeField] float speed;
     [SerializeField] float distanceFromTarget;
     [SerializeField] float targetUpdatRate;
@@ -13,17 +14,21 @@ public class NonPlayerActor : Actor
 
     private float targetTimer;
     private float smallWidth = 0.1f;
+    private float distanceToTarget;
     private Vector3 targetPoint;
+    private bool hasSeenTarget = false;
+
+
 
     protected override void Initalize()
     {
         base.Initalize();
+        targetPoint = target.FocusPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         targetTimer += Time.deltaTime;
 
         if (targetTimer >= targetUpdatRate)
@@ -33,24 +38,54 @@ public class NonPlayerActor : Actor
             PlotMove();
         }
 
-
-        Vector3 heading = targetPoint - transform.position;
-        float distance = Vector3.Distance(transform.position, targetPoint);
-        if (distance < detectionRange && distance > distanceFromTarget + smallWidth)
+        if (hasSeenTarget)
         {
-            transform.position += heading.normalized * speed * Time.deltaTime;
-        }
-        else if (distance < distanceFromTarget - smallWidth)
-        {
-            transform.position += -heading.normalized * speed * Time.deltaTime;
-        }
+            Vector3 heading = targetPoint - transform.position;
+            distanceToTarget = Vector3.Distance(transform.position, targetPoint);
+            if (distanceToTarget < detectionRange && distanceToTarget > distanceFromTarget + smallWidth)
+            {
+                transform.position += heading.normalized * speed * Time.deltaTime;
+            }
+            else if (distanceToTarget < distanceFromTarget - smallWidth)
+            {
+                transform.position += -heading.normalized * speed * Time.deltaTime;
+            }
 
-        transform.LookAt(target.FocusPoint.position);
+            transform.LookAt(target.FocusPoint.position);
+
+            AttemptFire();
+        }
+        else
+        {
+            if (CanSeeTarget())
+            {
+                hasSeenTarget = true;
+            }
+        }
     }
 
     protected virtual void PlotMove()
     {
         targetPoint = target.FocusPoint.position;
+    }
+
+    void AttemptFire()
+    {
+
+        if (CanSeeTarget())
+        weapon.Shoot(targetPoint);
+    }
+
+    bool CanSeeTarget()
+    {
+        if (distanceToTarget > detectionRange)
+            return false;
+        if (Vector3.Angle(this.transform.forward, targetPoint - this.transform.position) > detectionFieldOfView)
+            return false;
+        if (Physics.Linecast(this.transform.position, targetPoint))
+            return false;
+
+        return true;
     }
 }
 
