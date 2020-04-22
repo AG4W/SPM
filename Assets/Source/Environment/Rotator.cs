2f7fbbcf87b5bc7f14df23ff6bc45c069c83e8d3
AG4W <lightarts@live.se>
2020-04-22 21:23:07 +0200

    Add rotator component.

---
+++ Assets/Source/Environment/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]float speed = 5f;
+    [SerializeField]Vector3 rotation = new Vector3(0f, 1f, 1f);
+
+    void Update()
+    {
+        this.transform.eulerAngles += rotation.normalized * speed * Time.deltaTime;
+    }
+}
