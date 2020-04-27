db7cad67847a69a9fc22c5d45a10b3d040d3b35f
AG4W <lightarts@live.se>
2020-04-27 02:27:15 +0200

    Add some mats for third level.

---
+++ Assets/Source/Temp/HotkeyMenuController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HotkeyMenuController : MonoBehaviour
+{
+    [SerializeField]GameObject menu;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            menu.SetActive(!menu.activeSelf);
+    }
+}
