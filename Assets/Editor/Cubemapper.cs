bf4d37641df873964b1949ac596cce8fc60f311b
AG4W <lightarts@live.se>
2020-05-10 17:14:27 +0200

    Mergew

---
+++ Assets/Editor/Cubemapper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using ICSharpCode.NRefactory.Ast;
+
+public class Cubemapper : ScriptableWizard
+{
+    [SerializeField]Camera camera;
+    [SerializeField]int resolution = 1024;
+
+    void OnWizardUpdate()
+    {
+        this.helpString = "Select camera position to render from";
+        this.isValid = (camera != null);
+    }
+    void OnWizardCreate()
+    {
+        Cubemap cm = new Cubemap(resolution, TextureFormat.ARGB32, false);
+        camera.RenderToCubemap(cm);
+        AssetDatabase.CreateAsset(cm, $"Assets/Content/Textures/Cubemaps/{camera.name}.cubemap");
+    }
+
+    [MenuItem("Tools/Cubemapper")]
+    static void CreateCubemap()
+    {
+        DisplayWizard<Cubemapper>("Create Cubemap", "Render");
+    }
+}
---
+++ Assets/Source/Environment/BlinkEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class BlinkEffect : MonoBehaviour
+{
+    [SerializeField]float onTime;
+    [SerializeField]float offTime;
+
+    [SerializeField]bool startOn = true;
+
+    [SerializeField]GameObject[] objects;
+
+    float timer;
+    float threshold;
+
+    void Awake()
+    {
+        threshold = startOn ? onTime : offTime;
+
+        for (int i = 0; i < objects.Length; i++)
+            objects[i].SetActive(startOn);
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= threshold)
+            Toggle();
+    }
+    void Toggle()
+    {
+        for (int i = 0; i < objects.Length; i++)
+            objects[i].SetActive(!objects[i].activeSelf);
+
+        threshold = threshold == onTime ? offTime : onTime;
+        timer = 0f;
+    }
+}
