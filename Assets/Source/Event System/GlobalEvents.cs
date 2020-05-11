0342b9344cb50fbdb6c767b713a7bf1735e28025
AG4W <lightarts@live.se>
2020-05-11 04:27:06 +0200

    Refactor names in GlobalEvents.cs

--- Assets/Source/Event System/GlobalEvents.cs
+++ Assets/Source/Event System/GlobalEvents.cs
@@ -3,85 +3,63 @@ using System.Collections.Generic;
 
 public static class GlobalEvents
 {
-    static List<Action<object[]>>[] events;
-    static Dictionary<Actor, List<Action<object[]>>[]> actorEvents;
+    static List<Action<object[]>>[] global;
+    //look upon me and despair
+    static Dictionary<Actor, List<Action<object[]>>[]> actor;
 
-    //orkade inte sätta upp en initializer, så lazy initar arrayen
     public static void Initialize()
     {
-        events = new List<Action<object[]>>[Enum.GetNames(typeof(GlobalEvent)).Length];
-        actorEvents = new Dictionary<Actor, List<Action<object[]>>[]>();
+        global = new List<Action<object[]>>[Enum.GetNames(typeof(GlobalEvent)).Length];
+        actor = new Dictionary<Actor, List<Action<object[]>>[]>();
 
-        for (int i = 0; i < events.Length; i++)
-            events[i] = new List<Action<object[]>>();
+        for (int i = 0; i < global.Length; i++)
+            global[i] = new List<Action<object[]>>();
     }
 
-    public static void Subscribe(GlobalEvent e, Action<object[]> a)
+    public static void Subscribe(GlobalEvent globalEvent, Action<object[]> action) => global[(int)globalEvent].Add(action);
+    public static void Subscribe(this Actor actor, ActorEvent actorEvent, Action<object[]> action)
     {
-
-        events[(int)e].Add(a);
-    }
-    public static void Unsubscribe(GlobalEvent e, Action<object[]> a)
-    {
-        events[(int)e].Remove(a);
-    }
-
-
-    //look upon me and despair
-    public static void Subscribe(this Actor actor, ActorEvent e, Action<object[]> action)
-    {
-
         //skapa ny slot ifall actor inte finns
-        if (!actorEvents.ContainsKey(actor))
+        if (!GlobalEvents.actor.ContainsKey(actor))
         {
             List<Action<object[]>>[] entries = new List<Action<object[]>>[Enum.GetNames(typeof(ActorEvent)).Length];
 
             for (int i = 0; i < entries.Length; i++)
                 entries[i] = new List<Action<object[]>>();
 
-            actorEvents.Add(actor, entries);
+            GlobalEvents.actor.Add(actor, entries);
         }
 
-        actorEvents[actor][(int)e].Add(action);
-    }
-    public static void Unsubscribe(this Actor actor, ActorEvent e, Action<object[]> action)
-    {
-        actorEvents[actor][(int)e].Remove(action);
-    }
-    public static void ClearAllListeners(this Actor actor)
-    {
-        actorEvents.Remove(actor);
+        GlobalEvents.actor[actor][(int)actorEvent].Add(action);
     }
 
-    public static void Raise(GlobalEvent e, params object[] args)
-    {
+    //oanvända, men sparar ifall de kommer behövas någon gång
+    //public static void Unsubscribe(GlobalEvent e, Action<object[]> a) => events[(int)e].Remove(a);
+    //public static void Unsubscribe(this Actor actor, ActorEvent e, Action<object[]> action) => actorEvents[actor][(int)e].Remove(action);
+    //public static void ClearAllListeners(this Actor actor) => actorEvents.Remove(actor);
 
-        for (int i = 0; i < events[(int)e].Count; i++)
+    public static void Raise(GlobalEvent globalEvent, params object[] args)
+    {
+        for (int i = 0; i < global[(int)globalEvent].Count; i++)
         {
-            //kommer krascha om vi invokar en null action
+            //kommer nullreffa om vi invokar en null action
             //också dumt om vi har tomma subscribes, så ta bort dem
-            if (events[(int)e][i] == null)
-                events[(int)e].RemoveAt(i);
+            if (global[(int)globalEvent][i] == null)
+                global[(int)globalEvent].RemoveAt(i);
 
-            events[(int)e][i].Invoke(args);
+            global[(int)globalEvent][i].Invoke(args);
         }
     }
     //vem behöver koll på vad som skickas vart oavsett
-    public static void Raise(this Actor actor, ActorEvent e, params object[] args)
+    public static void Raise(this Actor actor, ActorEvent actorEvent, params object[] args)
     {
-
-        for (int i = 0; i < actorEvents[actor][(int)e].Count; i++)
+        for (int i = 0; i < GlobalEvents.actor[actor][(int)actorEvent].Count; i++)
         {
-            if (actorEvents[actor][(int)e][i] == null)
-                actorEvents[actor][(int)e].RemoveAt(i);
+            if (GlobalEvents.actor[actor][(int)actorEvent][i] == null)
+                GlobalEvents.actor[actor][(int)actorEvent].RemoveAt(i);
 
-            actorEvents[actor][(int)e][i].Invoke(args);
+            GlobalEvents.actor[actor][(int)actorEvent][i].Invoke(args);
         }
-    }
-
-    internal static void Raise(string v)
-    {
-        throw new NotImplementedException();
     }
 }
 
@@ -125,7 +103,7 @@ public enum GlobalEvent
     NoiseCreated,
     AlertOthers,
 
-    //SceneLoad
+    //scene management
     OnSceneLoad,
 }
 public enum ActorEvent
