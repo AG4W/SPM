3f5b9662d1f9d5a30e0f259bd1163ad4eb4a4637
AG4W <lightarts@live.se>
2020-05-14 23:32:42 +0200

    Update AI

---
+++ Assets/Source/State Machine/States/AI/AIMoveToCoverState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "States/AI/Move To Cover")]
+public class AIMoveToCoverState : AIBaseState
+{
+    public override void Enter()
+    {
+        //locate best cover position here
+    }
+    public override void Tick()
+    {
+    }
+    public override void Exit()
+    {
+    }
+}
