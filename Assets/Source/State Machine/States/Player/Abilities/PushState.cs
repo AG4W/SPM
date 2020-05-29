ab1f65496384889c67b10c6ba23da92622d4a79e
frilel <crillefrille@gmail.com>
2020-05-29 16:51:46 +0200

    Update det som blev revertat...

--- Assets/Source/State Machine/States/Player/Abilities/PushState.cs
+++ Assets/Source/State Machine/States/Player/Abilities/PushState.cs
@@ -6,6 +6,8 @@ public class PushState : AbilityState
     [SerializeField]float radius = 4f;
     [SerializeField]float distance = 15f;
     [SerializeField]float power = 50f;
+    [Tooltip("Defines the cone infront of the player in which objects should be affected by the force")]
+    [SerializeField] [Range(-1f, 0f)] float angularThreshold = -.25f;
 
     [SerializeField]AnimationCurve initialAcceleration;
 
@@ -22,7 +24,7 @@ public class PushState : AbilityState
         for (int i = 0; i < hits.Length; i++)
         {
             if (hits[i].transform != null)
-                if (hits[i].transform.GetComponent<IForceAffectable>() != null)
+                if (hits[i].transform.GetComponent<IForceAffectable>() != null && Vector3.Dot(base.Actor.transform.forward, base.Actor.transform.position.DirectionTo(hits[i].transform.position)) >= angularThreshold)
                 hits[i].transform.GetComponent<IForceAffectable>().ModifyVelocity(ray.direction.normalized * (base.Timer >= 1f ? power : initialAcceleration.Evaluate(base.Timer) * power));
         }
     }
