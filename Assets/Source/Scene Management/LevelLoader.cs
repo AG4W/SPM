e00a035f6e65f43277f80060f53ba91940ef665c
frilel <crillefrille@gmail.com>
2020-06-04 08:50:10 +0200

    Update hotfix levelload checkpoint null

--- Assets/Source/Scene Management/LevelLoader.cs
+++ Assets/Source/Scene Management/LevelLoader.cs
@@ -7,6 +7,7 @@ public class LevelLoader : MonoBehaviour
     public void LoadLevel(int sceneNr)
     {
         //GlobalEvents.Raise(GlobalEvent.SetPlayerCurrentCheckpoint, null);
+        FindObjectOfType<PlayerActor>().clearCheckPoint();
         GlobalEvents.Raise(GlobalEvent.OnSceneExit, SceneManager.GetActiveScene());
         SceneManager.LoadScene(sceneNr);
     }
