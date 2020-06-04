ff178d28acea059bd19db25fe43815cd72edc132
frilel <crillefrille@gmail.com>
2020-06-04 05:56:21 +0200

    Update levelLoader.cs

--- Assets/Source/Scene Management/LevelLoader.cs
+++ Assets/Source/Scene Management/LevelLoader.cs
@@ -3,9 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    public void LoadLevel(string scenename)
+
+    public void LoadLevel(int sceneNr)
     {
+        //GlobalEvents.Raise(GlobalEvent.SetPlayerCurrentCheckpoint, null);
         GlobalEvents.Raise(GlobalEvent.OnSceneExit, SceneManager.GetActiveScene());
-        SceneManager.LoadScene(scenename);
+        SceneManager.LoadScene(sceneNr);
     }
 }
